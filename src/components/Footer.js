c4ba2120da66bd0ea91451d8fee21113ee25e012
rashid.u <rashidsuv11@gmail.com>
2025-07-07 23:11:42 +0530

    basic project start

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-white border-t p-4 text-center">
+      <p className="text-[#4C4C4C] !font-extralight text-[0.9rem]">
+        &copy; All Rights Reserved | Designed And Developed By MKM Solutions
+      </p>
+    </footer>
+  );
+}

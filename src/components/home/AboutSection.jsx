6909e719389e13654891ec2e6a0a2dbd12668067
rashid.u <rashidsuv11@gmail.com>
2025-08-03 19:42:33 +0530

    product section done

--- src/components/home/AboutSection.jsx
+++ src/components/home/AboutSection.jsx
@@ -1,4 +1,5 @@
 import { useTranslations } from "next-intl";
+import KeySellingPointsSection from "../common/KeySellingPointsSection";
 
 export function AboutSection() {
   const t = useTranslations("HomePage.HomeAbout");
@@ -44,44 +45,7 @@ export function AboutSection() {
       </div>
 
       {/* Icons Row */}
-      <div className="bg-primary text-white mt-10 sm:mt-16 py-6 sm:py-10 pl-3 sm:pl-3 md:pl-0">
-        <div className="max-w-4xl mx-auto grid grid-cols-4 gap-2 px-4">
-          <div className="flex flex-col items-center">
-            <img
-              src="/images/parts.png"
-              alt="Affordable Auto Parts"
-              className="w-8 md:w-15 h-8 md:h-15 mb-5"
-            />
-            <p className="text-[10px] md:text-sm whitespace-nowrap">
-              {t("icons.parts")}
-            </p>
-          </div>
-          <div className="flex flex-col items-center">
-            <img
-              src="/images/brand.png"
-              alt="Top Brands"
-              className="w-9 md:w-17 h-8 md:h-15 mb-5"
-            />
-            <p className="text-[10px] md:text-sm">{t("icons.brand")}</p>
-          </div>
-          <div className="flex flex-col items-center">
-            <img
-              src="/images/quality.png"
-              alt="Trusted Quality"
-              className="w-8 md:w-15 h-8 md:h-15 mb-5"
-            />
-            <p className="text-[10px] md:text-sm">{t("icons.quality")}</p>
-          </div>
-          <div className="flex flex-col items-center">
-            <img
-              src="/images/delivery.png"
-              alt="Secure Delivery"
-              className="w-8 md:w-15 h-8 md:h-15 mb-5"
-            />
-            <p className="text-[10px] md:text-sm">{t("icons.delivery")}</p>
-          </div>
-        </div>
-      </div>
+      <KeySellingPointsSection />
     </section>
   );
 }

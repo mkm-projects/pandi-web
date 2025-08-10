696ca99708683ab0b512b5dbb3c257f555b0a449
rashid.u <rashidsuv11@gmail.com>
2025-08-10 11:38:01 +0530

    home page, font and layout issues fixed

--- src/components/home/AboutSection.jsx
+++ src/components/home/AboutSection.jsx
@@ -1,5 +1,6 @@
 import { useTranslations } from "next-intl";
 import KeySellingPointsSection from "../common/KeySellingPointsSection";
+import Link from "next/link";
 
 export function AboutSection() {
   const t = useTranslations("HomePage.HomeAbout");
@@ -7,22 +8,22 @@ export function AboutSection() {
   return (
     <section className="relative bg-white text-center">
       {/* Stats Row */}
-      <div className="-mt-13 z-10 relative bg-primary text-white rounded-xl shadow-md flex justify-around items-center py-6 md:py-8 px-6 w-[90%] md:w-[70%] mx-auto">
+      <div className="-mt-13 z-10 relative bg-primary text-white rounded-xl shadow-md flex justify-around items-center py-6 md:py-8 px-6 w-[90%] md:w-[70%] mx-auto gap-2 ">
         <div>
-          <p className="text-1xl md:text-3xl font-bold">53+</p>
-          <p className="text-[10px] md:text-sm mt-1 md:mt-3">
+          <p className="text-2xl md:text-3xl font-bold">53+</p>
+          <p className="text-[10px] md:text-2xl whitespace-nowrap mt-1 md:mt-3">
             {t("counts.yearsOfExperience")}
           </p>
         </div>
         <div>
-          <p className="text-1xl md:text-3xl font-bold">50M+</p>
-          <p className="text-[10px] md:text-sm mt-1 md:mt-3">
+          <p className="text-2xl md:text-3xl font-bold">50M+</p>
+          <p className="text-[10px] md:text-2xl whitespace-nowrap mt-1 md:mt-3">
             {t("counts.orderServedSuccess")}
           </p>
         </div>
         <div>
-          <p className="text-1xl md:text-3xl font-bold">10M+</p>
-          <p className="text-[10px] md:text-sm mt-1 md:mt-3">
+          <p className="text-2xl md:text-3xl font-bold">10M+</p>
+          <p className="text-[10px] md:text-2xl whitespace-nowrap mt-1 md:mt-3">
             {t("counts.satisfiedCustomers")}
           </p>
         </div>
@@ -33,15 +34,17 @@ export function AboutSection() {
         <h2 className="font-bold text-3xl sm:text-5xl md:text-6xl leading-[100%] text-center capitalize mb-4">
           {t("about")} <span className="text-primary">{t("us")}</span>
         </h2>
-        <p className="text-[#4C4C4C] font-light text-[14px] sm:text-[16px] leading-[30px] sm:leading-[34.5px] text-center capitalize mb-6">
+        <p className="text-gray-700 font-light text-md md:text-xl leading-relaxed fadeIn mb-6">
           {t("p1")}
         </p>
-        <p className="text-[#4C4C4C] font-light text-[14px] sm:text-[16px] leading-[30px] sm:leading-[34.5px] text-center capitalize mb-6">
+        <p className="text-gray-700 font-light text-md md:text-xl leading-relaxed fadeIn mb-8">
           {t("p2")}
         </p>
-        <button className="bg-primary text-white px-6 py-3 rounded-full font-medium hover:opacity-90 transition">
-          {t("callAction")}
-        </button>
+        <Link href="/about">
+          <button className="bg-primary text-white px-6 py-3 rounded-full font-light hover:opacity-90 transition">
+            {t("callAction")}
+          </button>
+        </Link>
       </div>
 
       {/* Icons Row */}
--- src/components/home/BrandSection.jsx
+++ src/components/home/BrandSection.jsx
@@ -6,23 +6,23 @@ export function BrandSection() {
   const t = useTranslations("HomePage.HomeBrand");
 
   return (
-    <section className="py-2 md:py-10 bg-white">
+    <section className="py-8 md:py-16 bg-white">
       <div className="px-4 max-w-6xl mx-auto">
-        <h2 className="font-bold text-3xl sm:text-5xl md:text-6xl leading-[100%] capitalize">
+        <h2 className="font-bold text-3xl sm:text-5xl md:text-6xl leading-tight capitalize">
           {t("brand")} <span className="text-primary">{t("trus")}</span>
         </h2>
-        <p className="text-[#4C4C4C] font-light text-[14px] sm:text-[16px] leading-[30px] sm:leading-[34.5px] capitalize max-w-2xl mt-8">
+        <p className="text-md md:text-xl font-light text-gray-700 leading-relaxed fadeIn max-w-2xl mt-6 md:mt-8">
           {t("p1")}
         </p>
       </div>
 
-      <div className="grid grid-cols-3 md:grid-cols-4 gap-4 md:gap-8 px-4 max-w-6xl mx-auto my-8 md:my-15">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 sm:gap-4 md:gap-6 lg:gap-8 px-4 max-w-6xl mx-auto mt-8 md:mt-12 lg:mt-16">
         {brandList.map((item) => (
           <div
             key={item.key}
-            className="bg-gray-100 rounded-xl p-6 text-center shadow-sm hover:shadow-lg transition"
+            className="bg-gray-100 rounded-xl p-3 sm:p-4 md:p-6 text-center shadow-sm hover:shadow-lg transition-shadow duration-300"
           >
-            <div className="relative w-15 md:w-30 h-9 md:h-18 mx-auto">
+            <div className="relative w-16 sm:w-20 md:w-24 lg:w-32 h-8 sm:h-10 md:h-12 lg:h-16 mx-auto">
               <Image
                 src={item.image}
                 alt={item.key}
